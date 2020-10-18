55f3bf0f29b8d3ffbaa1ba28fbf449724394c872
Muhammad Bilal <ExpBilal@gmail.com>
2020-10-18 12:23:57 +0500

    Second  commit

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
+import React , { useState } from 'react'
 import classes from './Nav.module.css'
 import Logo from './logo.png'
 export const Nav = ({NcolorBG , NtextColor}) => {
 
+    const [openMenu, setopenMenu] = useState(false)
+    const OnClickNavButton = () => {
+        setopenMenu(openMenu ? false : true);
+    }
     return (
+        <>
         <nav style={{backgroundColor: NcolorBG }} className={classes.Navbar} >   
-            {console.log(NcolorBG,NtextColor)}
          <img src={Logo} alt=''/>
-         <ul>
-            <li><a href='#item-1' alt='' style={{color: NtextColor}}>Ground-Truth Data</a></li>
-            <li><a href='#item-2' alt='' style={{color: NtextColor}}>How its Works</a></li>
-            <li><a href='#item-3' alt='' style={{color: NtextColor}}>Fee Structure</a></li>
-            <li><a href='#item-4' alt='' style={{color: NtextColor}}>Example Deliverables</a></li>
-            <li><a href='#item-5' alt='' style={{color: NtextColor}}>Submission Portal</a></li>
-            <li><a href='#item-2' alt='' style={{color: NtextColor}}>Contact Us</a></li>
-            <li><button className={classes.Navbutton}>Sign In</button></li>
-        </ul>         
+        {
+                <ul className = {classes.showUL}>
+                     <li><a href='#item-1' alt='' style={{color: NtextColor}}>Ground-Truth Data</a></li>
+                     <li><a href='#item-2' alt='' style={{color: NtextColor}}>How its Works</a></li>
+                     <li><a href='#item-3' alt='' style={{color: NtextColor}}>Fee Structure</a></li>
+                     <li><a href='#item-4' alt='' style={{color: NtextColor}}>Example Deliverables</a></li>
+                     <li><a href='#item-5' alt='' style={{color: NtextColor}}>Submission Portal</a></li>
+                     <li><a href='#item-2' alt='' style={{color: NtextColor}}>Contact Us</a></li>
+                     <li><button className={classes.Navbutton}>Sign In</button></li>
+                 </ul>
+        }
+        <div className = {classes.navbarmenu} onClick = {OnClickNavButton}>
+        <span></span>
+        <span></span>
+        <span></span>
+        </div>        
         </nav>
-    )
+                <ul className = {openMenu ? classes.showMenu : false}>
+                <li><a href='#item-1' alt='' style={{color: "black"}}>Ground-Truth Data</a></li>
+                <li><a href='#item-2' alt='' style={{color: "black"}}>How its Works</a></li>
+                <li><a href='#item-3' alt='' style={{color: "black"}}>Fee Structure</a></li>
+                <li><a href='#item-4' alt='' style={{color: "black"}}>Example Deliverables</a></li>
+                <li><a href='#item-5' alt='' style={{color: "black"}}>Submission Portal</a></li>
+                <li><a href='#item-2' alt='' style={{color: "black"}}>Contact Us</a></li>
+                <li><button className={classes.Navbutton}>Sign In</button></li>
+            </ul>
+        </>
+)
 }
+
